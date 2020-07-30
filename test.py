3264e1e6d7e55e72dda877b60f3a790f7f75353c
NgChowYong <ngchowyong@hotmail.com>
2020-07-30 12:42:20 +0800

    update for test file to test multithreading

--- test.py
+++ test.py
@@ -1,2 +1,29 @@
-print("hello from PC")
+# print("hello from PC")
 # $ scp test.py root@192.168.0.240:/home/root/test.py
+
+# test for multithreading
+import threading
+import time
+
+class MyThread(threading.Thread):
+  def __init__(self, num):
+    threading.Thread.__init__(self)
+    self.num = num
+
+  def run(self):
+    print("job start")
+    time.sleep(self.num)
+    print("job end")
+
+t = MyThread(0.1)
+
+t.start()
+
+print("main start")
+time.sleep(2)
+print("main end")
+
+# wait until job finished then continue
+t.join()
+
+print("all end")
